110115c0280e8f35f137364a8fd035cbca9981d7
smantoor <smantoor@SMANTOOR-BDJDF94>
2025-09-16 15:09:33 +0530

    Testing custom tags

--- weblogic-azure-vm/arm-oraclelinux-wls-admin/src/main/scripts/postDeploymentScript.sh
+++ weblogic-azure-vm/arm-oraclelinux-wls-admin/src/main/scripts/postDeploymentScript.sh
@@ -1,3 +1,20 @@
+#!/bin/bash
+# Copyright (c) 2021, Oracle and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+#Function to output message to StdErr
+function echo_stderr ()
+{
+    echo "$@" >&2
+}
+
+#Function to display usage message
+function usage()
+{
+  echo_stderr "./postDeploymentScript.sh <<< \"<wlsDomainSetupArgsFromStdIn>\""
+}
+
+
 echo "Executing post Deployment script"
 az account show
 PUBLIC_IPS="$(az network public-ip list --resource-group ${RESOURCE_GROUP_NAME})"
